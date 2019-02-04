72a881b6e7a33564cab08b7dd443f8b63975ad73
kfang2 <47185626+kfang2@users.noreply.github.com>
2019-02-04 11:03:24 -0500

    Create kunfang.php

---
+++ lab02/kunfang.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Kun Fang|Lab1: Initial Personal Website</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body class="container">
+
+<header>
+
+  <a href="index.php">
+  	<h1>Kun Fang</h1>
+	<img src="images/IMG.jpg" alt="Kun Fang">
+	</a>
+</header>
+
+
+<aside>
+	<ul>
+		<li>ShenZhen,GuangDong,China</li>
+		<li>DMS&Business</li>
+		<li>Amateur in Web Design</li>
+	</ul>
+	</aside>
+
+<main>
+	<h2> My Major</h2>
+	
+	<p>However, it was not my intention to major in neither Digital Media Studies or Business at the first place. When I was in high school, my favorite subject was history. I enjoyed the process of  reading and thinking. After I successfully became the president of student union, my passion towards leadership and politics gradually grow in my body. I then get an internship in Congresswoman Mimi Walters’ local district office and applied for University of Rochester as a Political Science Major student. </p>
+
+	<h2> My path toward DMS</h2>
+	<p>Things seem to go smoothly, nevertheless, I knew something goes wrong after the first semester. Chronic tardiness lets my grade down and reveals the lack of interest. In the following semester, I tried out a lot of distinctively different courses. And eventually find myself in marketing course. My crew and I was told to develop a brand new marketing plan and write down a paper for an existing product. Although writing down the ideas are useful, I realized I need more skills to really make the plan happens. Things like designing a more stylish logo or improving the overall quality of its’ website require actual skills. I decided to further my knowledge, and therefore taking a few classes toward web design and digital media programming. Right now, I am still an amateur. But I want to explore more in the rest of my college life.</p>
+
+	<h2> On my spare time</h2>
+	<p>In addition, I have been learning piano since 3 years-old. If I could get a chance, I wish to take some AME courses and learn about sound recording. I also like to watch action and horror movies. The excitement you can get from these kinds of movies is like a dagger that strikes into audiences’ heart. I do want to know about the story behind the screen.I love skating, skiing, playing badminton for sports. During vacation time, I would spend a lot of my time trying all kind of extreme sports. Right now I have already completed my mission in paragliding and skydiving. I am really looking forward to bungee jumping one day in the future.</p>
+</main>
+<footer>
+	© KUN FANG • CSC 174 - Advanced Front-end Web Design and Development
+</footer>
+
+<?php
+
+include "inc/nav.inc";
+  
+?>
+
+
+
+
+</body>
+
+ <script src="https://code.jquery.com/jquery.js"></script>
+<script src="js/menu-highlighter.js"></script>
+ 
+
+</html>
