f932a63efd76cf6b6ee57a3ae8900028c8be2c45
qfu4 <qfu4@u.rochester.edu>
2019-02-04 11:03:24 -0500

    Add files
    images * 2
    php pages * 2
    inc * 1

---
+++ lab02/qianwen-fu.php
@@ -0,0 +1,43 @@
+<?php
+$currentPage="Qianwen Fu | Lab 2: Flash Website";
+include "inc/html-top.inc";?>
+
+	<?php include "inc/aside.inc";?>
+
+	<header>
+		<a href="http://qfu4.digitalscholar.rochester.edu/csc174/lab01/index.php">
+			<h1>Qianwen Fu</h1>
+			<img src="images/qianwen.jpeg" alt="a picture of Qianwen Fu">
+		</a>
+	</header>
+	
+	<aside>
+		<ul>
+			<li>First Major: Digital Media Studies</li>
+			<li>Second Major: Business Analytics</li>
+			<li>Ramen & Matcha Lover</li>
+
+		</ul>
+    </aside>
+    
+	<main>
+		<h2>Hometown</h2>
+			<p>I come from a small city in Guangdong Province, an area at Southeast China. A common stereotype says that people from there eat everything, especially their neighbors from Fujian Province. I would say this statement is totally untrustworthy, as we never have those crazy things in our bowls, but we indeed enjoy delicious cuisines and receive fewer pocket money than kids from other provinces. Although I moved to Beijing, a city has cold winter, with my family at the age of three, I feel that the love of sub-tropical climate is written in my genes. I still do not quite understand why I came to the UR, a place with extreme winters. Sometimes I wonder if I was cheated by the beautiful photo of UR’s summer on the admission booklet. Sometimes I really want to head to California or Florida when I see the photos from my friends studying there, but when I heard about the rigid general education credit requirement at their colleges, I believe the UR is still the best fit for me.</p>
+		<h2>My Major</h2>
+			<p>When I entered my high school, I was determined to become a chemistry or biology major in the future. I took related AP courses, tests, and competitions in high school, trying to prepare myself for this goal. However, I found that my interests were not enough to keep myself continuing exploring these fields in my university and even in my future career. I love literature and arts: love reading about interesting news and stories and sharing them out and enjoy visiting galleries and museums to explore the histories behind them. I like creating dances by myself, learning about how to edit videos or take good photos, and trying to draw cartoon characters on my sketchbook. Therefore, I chose digital media studies, a major with a lot of areas that I am interested to touch upon. Although I may not be good at all of them, I am willing to learning about them as a hobby or a technique that can be contributed to my life or my job in the future.</p>
+
+		<h2>This Semester</h2>
+			<p>This semester, I am looking forward to learning more about designing beautiful and user-friendly web pages, and I am excited about creating web pages with my friends. I will try to avoid procrastination, or my partners might post my ugly photos on our webpage (just joking). Beside CSC174, I am also taking ENG118 for DMS this semester. As a slow reader and writer, I think I will cry when I get hundreds of pages of reading materials or tons of writing assignments. Anyways, life is still going, and I have my friends.❤️</p>
+
+	</main>
+
+	<footer>CSC174: Advanced Front-end Web Design and Development</footer>	
+	<nav>
+
+		
+
+</nav>	
+	<script src="./qianwen fu_files/jquery.js"></script>
+<script src="./qianwen fu_files/menu-highlighter.js"></script>	
+
+</body></html>
