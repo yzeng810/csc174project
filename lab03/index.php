2aec64d8612451d2a3b96d73064daacb49990547
Yuqi Zeng <yuqizeng@YuqideMacBook.local>
2019-02-10 17:22:12 -0500

    update

---
+++ lab03/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>When Novel Meets Fashion | Lab 3: Smash Website | Team Hong Kong</title>
+	<link rel="stylesheet" href="css/home.css">
+</head>
+
+<body class="container">
+
+	<header>
+			<h1>When Novel Meets Fashion</h1>
+			<sub>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</sub>
+	</header>
+
+	<main>
+
+		<section class="excerpt">
+		<div class="text">
+		<h2>Novel</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<a href="novel.php">Read More</a>
+		</div>
+		<img src="images/novel.jpg">
+		</section>
+
+		<section class="excerpt">
+		<div class="text">
+		<h2>High-Street Fashion</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<a href="sfashion.php">Read More</a>
+		</div>
+		<img src="images/sfashion.jpg">
+		</section>
+
+		<section class="excerpt">
+		<div class="text">
+		<h2>Designer House</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<a href="designerhouse.php">Read More</a>
+		</div>
+		<img src="images/dhouse.jpg">
+		</section>
+		
+	</main>
+
+	<footer>CSC 174: Advanced Front-end Web Design and Development</footer>
+
+	<?php include "inc/nav.inc"; ?>
+
+<?php include "inc/scripts.inc"; ?>
+
+</body>
+
+</html>
---
+++ lab03/kunfang.md
@@ -0,0 +1,24 @@
+CSC174 Lab 3: Markdown and Information Architecture|Kun Fang|Feburary 6th
+======
+
+## Everlane 
+-Everlane is a United States clothing retailer that sells primarily online. The organization is headquartered in San Francisco, California and also has a store in New York City.
+
+Features:
+* Enviromentally friendly 
+* Choose what you pay 
+
+## Forever 21
+
+-Forever 21, stylized as FOREVER 21, is an American fast fashion retailer headquartered in Los Angeles, California. Forever 21 began as the store called Fashion 21 with 900 square feet (84 m2) in Highland Park, Los Angeles, in 1984, and has grown into the clothing lines Forever 21, XXI Forever, Love 21 and Heritage with over 600 stores in the Americas, Asia, the Middle East and the UK
+
+Features:
+* Cover huge areas
+* Quickly outdate/per 2 weeks 
+
+## American Eagles
+-American Eagle Outfitters, Inc., now known as simply American Eagle, is an American lifestyle clothing and accessories retailer, headquartered in the Southside Works Neighborhood of Pittsburgh, Pennsylvania.
+
+Feature:
+* Comfort that comes with fashion 
+* Fit all kinds of body sizes
